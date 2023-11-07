f5d265fdca97612a909da5a834ceb1370917f9d2
alan <alan.fer09@gmail.com>
2023-11-07 20:48:11 -0300

    add

--- app.js
+++ app.js
@@ -7,10 +7,49 @@ const mongoose = require('mongoose')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 
+//midlewares
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json())
 app.set('view engine', 'ejs');
+
+app.get('/',(req,res)=>{
+    const userId = req.cookies.userId
+    if(userId){
+        res.redirect('/profile')
+    }else{
+        res.redirect('/login')
+    }
+})
+
+app.get('/profile',async (req,res)=>{
+    const userId = req.cookies.userId
+    const user = await User.findById(userId)
+    res.render('pages/profile',{user:user})
+})
+
+app.get('/login',(req,res)=>{
+    res.render('pages/login')
+})
+
+app.post('/auth/login',async (req,res)=>{
+    try{
+        console.log('body:',req.body)
+        const email = req.body.email
+        const password = req.body.password
+        const user = await User.findOne({email:email})
+        if(user && user.password === password){
+            const userId = JSON.stringify(user._id)
+            console.log('userId',userId)
+            res.cookie('UserId',userId)
+            res.status(200).send('OK')
+        }
+    }catch(error){
+        res.status(400).end()
+    }
+
+
+})
 
 
 
--- public/login.js
+++ public/login.js
@@ -1,1 +1,23 @@
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const form = document.querySelector('form')
+console.log('hola mundo')
 
+form.addEventListener('submit',(event)=>{
+    event.preventDefault()
+    fetch('http://localhost:3000/auth/login',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({
+            email:email.value,
+            password:password.value
+        })
+    }).then((data)=>{
+        console.log('data',document.cookie)
+        //window.location.href= "http://localhost:3000/profile"
+    }).catch((error)=>{
+        console.error('error-login',error)
+    })
+})
